fa63882628b440075b913b569d33fc1a370088ea
Tuomas Myllymäki <myllymakituomas1998@gmail.com>
2025-02-01 20:41:36 +0200

    Moved tested versions of 1-form, 0-form and poisson solver to own files

---
+++ poisson_solver.py
@@ -0,0 +1,23 @@
+from zero_form import ZeroForm
+import numpy as np
+
+def poisson_solve(Phi_form: ZeroForm) -> np.array:
+    Phi = Phi_form.scalar_field
+    h = Phi_form.h
+
+    Nx, Ny, Nz = Phi.shape
+    # Mapping into Fourier space:
+    Phi_fft = np.fft.fftn(Phi)
+    
+    # Calculate the necessary frequencies for each dimension:
+    K = np.asarray([[[[k_xi / Nx, k_yi / Ny, k_zi / Nz] for k_xi in range(Nx)] for k_yi in range(Ny)] for k_zi in range(Nz)])
+
+    # Divisor with finite difference taken into account:
+    sin_correction = np.sum(np.sin(np.pi * K) ** 2, axis = -1) * 4 / (h ** 2)
+    sin_correction[0, 0, 0] = 1
+    result = Phi_fft / (-sin_correction)
+    result[0, 0, 0] = 0
+
+    # Transform back into original space
+    result = np.fft.ifftn(result)
+    return result
