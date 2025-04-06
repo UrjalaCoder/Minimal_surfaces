44bbb04fdb2b0c0c523e99b7021e2dfc20bead39
Tuomas Myllymäki <myllymakituomas1998@gmail.com>
2025-04-06 19:13:18 +0300

    rewriting blog post 2.

--- src/animation/animate_field.py
+++ src/animation/animate_field.py
@@ -2,43 +2,61 @@ from manim import *
 from ..grid import Grid
 from ..vector_field import VectorField
 
-def map_to_camera_view(point: np.array) -> np.array:
-    return (point * 10) - 5
+def build_vector_field_scene(
+        grid: Grid,
+        vector_field: VectorField,
+        curve_function: callable = None,
+        surface_function: callable = None,
+        curve_color = BLACK,
+        surface_color = BLACK,
+        streak_color = BLUE,
+        background_color = WHITE,
+        streak_time = 0.8,
+        streak_opacity = 1.0,
+        dt = 0.05
+    ):
+    def map_to_camera_view(point: np.array) -> np.array:
+        return (point * 10) - 5
 
-def inverse_map_to_camera_view(point: np.array) -> np.array:
-    return (point + 5) / 10
-
-def build_vector_field_scene(grid: Grid, vector_field, curve_points,
-                              vector_scale=0.1, color=BLUE, max_vectors=1000, streak_time = 0.8, streak_opacity = 0.6):
+    def inverse_map_to_camera_view(point: np.array) -> np.array:
+        return (point + 5) / 10
     
     def mapping_of_vector_field(camera_position: np.array) -> np.array:
         mapped_position = inverse_map_to_camera_view(camera_position)
         return vector_field(mapped_position)
     
-    def mapping_of_curve(curve_points: np.array) -> np.array:
-        return np.array([map_to_camera_view(p) for p in curve_points])
+    def mapping_of_curve(t: float) -> np.array:
+        return map_to_camera_view(curve_function(t))
+    
+    def mapping_of_surface(u, t) -> np.array:
+        return map_to_camera_view(surface_function(u, t))
 
     class DiracStreamLineScene(ThreeDScene):
         def construct(self):
+            self.camera.background_color = background_color
             self.set_camera_orientation(phi=75 * DEGREES, theta=45 * DEGREES)
             self.camera.set_zoom(1.0)
 
             # Add the curve
-            mapped_curve_points = mapping_of_curve(curve_points)
-            curve = VMobject(color=WHITE).set_points_smoothly(mapped_curve_points)
-            self.add(curve)
+            if curve_function is not None:
+                curve = ParametricFunction(mapping_of_curve, t_range=(0, 1), color = curve_color)
+                self.add(curve)
+            
+            if surface_function is not None:
+                surface = Surface(mapping_of_surface, u_range=(0, 1), v_range=(0, 1), fill_opacity=0.8, fill_color=surface_color, checkerboard_colors=[surface_color])
+                self.add(surface)
 
             # Add streamlines
             stream_lines = StreamLines(
                 func=mapping_of_vector_field,
                 x_range=[-5, 5, 1],
                 y_range=[-5, 5, 1],
-                z_range=[-4, 4, 1],
+                z_range=[-3, 3, 1],
                 stroke_width=1.2,
                 virtual_time=streak_time,
-                color=color,
+                color=streak_color,
                 opacity=streak_opacity,
-                dt = 0.05
+                dt = dt
             )
             stream_lines.start_animation()
             self.add(stream_lines)
